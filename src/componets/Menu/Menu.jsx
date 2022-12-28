e367af02bf99a1af634f6ec5b31df7728985a5a6
Abhijith Jaideep <abhijithjaideep176@gmail.com>
2022-12-28 23:45:25 +0530

    Menu Created

---
+++ src/componets/Menu/Menu.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './menu.css'
+
+function Menu() {
+    const [open, setopen] = useState(false);
+
+    const toggleMenu = () => {
+        if (open === false) {
+            setopen(true)
+        }
+        else {
+            setopen(false)
+        }
+    }
+
+    return (
+        <>
+            <div className="menu">
+                <div className={`${open?'menubar':'menu-dismiss'}`}>
+                    <div className={`${open?'menuitems':'menu-dismiss'}`}>
+                        <li>Home</li>
+                        <li>Rules and guidelines</li>
+                        <li>FAQ</li>
+                        <li>Contact Us</li>
+
+                    </div>
+
+                    <div className={`${open?'links':'menu-dismiss'}`}>
+                        <p>facebook</p>
+                        <p>instagram</p>
+                        <p>twitter</p>
+                    </div>
+                </div>
+                
+                <div className="hamburger" onClick={toggleMenu}>
+                    <p className={`${open ? 'line' : ''}1 line`}></p>
+                    <p className={`${open ? 'line' : ''}2 line`}></p>
+                    <p className={`${open ? 'line' : ''}3 line`}></p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Menu
