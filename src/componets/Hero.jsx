272623f3e099ce975d50db473e2928359e2a75cb
17072001MUSAFIR <musafirky@gmail.com>
2022-12-29 15:12:43 +0530

    added contact us

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import ContactUs from './ContactUs'
 
 export default class Hero extends Component {
   render() {
     return (
       <div className='relative h-screen  bg-[#0F044C]'>
         <Navbar/>
+        <ContactUs/>
       </div>
     )
   }
