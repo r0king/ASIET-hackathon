42435eb4b7e6247a5aff73a456bf081cc5263949
Royal Babu <123royalbabu@gmail.com>
2022-12-29 15:11:13 +0530

    hero page responsive

--- src/componets/HeroTitle.jsx
+++ src/componets/HeroTitle.jsx
@@ -21,16 +21,17 @@ export default class HeroTitle extends Component {
   }
   render() {
     return (
-      <div className="h-screen w-screen flex justify-center content-center items-center" ref={this.element}>
+      <div className="h-screen w-screen flex pb-[33vh] justify-center content-center items-center mb-10" ref={this.element}>
         <h1
           style={{
             fontFamily: "Azonix",
           }}
-          className="text-4xl md:text-6xl text- text-center bg-clip-text bg-gradient-to-br from-[#ADB1C7] to-[#452308] font-extrabold text-transparent"
+          className="text-4xl max-w-[80vw] sm:text-6xl  text- text-center bg-clip-text bg-gradient-to-br from-[#ADB1C7] to-[#452308] font-extrabold text-transparent"
         >
-          HACKATHON
+          ADI SHANKARA HACKATHON 23
         </h1>
       </div>
     );
   }
 }
+
