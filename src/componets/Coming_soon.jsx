bd9252853f8692ff986522d6343fd08b6ec8667c
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2022-12-31 01:10:22 +0530

    centred coming soon in mobileview

--- src/componets/Coming_soon.jsx
+++ src/componets/Coming_soon.jsx
@@ -11,22 +11,23 @@ const ComingSoon = () => {
 
         {/* :TEXT BACKGROUND */}
         <div className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-          <p className="text-4xl font-extrabold text-center text-[#0F3D3E] text-opacity-25 sm:text-6xl md:text-7xl">Great Things</p>
-          <p className="font-black tracking-widest text-center text-[#0F3D3E] text-opacity-25 uppercase text-8xl sm:text-10xl md:text-12xl">Take Time</p>
+          <p className="text-4xl font-extrabold text-center text-[#0D6394] text-opacity-40 sm:text-6xl md:text-7xl">Great Things</p>
+          <p className="font-black tracking-widest text-center text-[#0D6394] text-opacity-40 uppercase text-8xl sm:text-10xl md:text-12xl">Take Time</p>
         </div>
 
 
         {/* :TEXT EFFECT */}
-        <div className="relative ">
+        <div className='adjust'>
+        <div className="relative mb-16">
           <TextEffect6>Coming Soon</TextEffect6>
         </div>
 
 
         {/* :BUTTON BACK HOME */}
-        <a href="#link" className="mt-16 text-base adjust">
+        <a href="#link" className="mt-16 text-base">
           <ButtonEffect34>Go Back to Home</ButtonEffect34>
         </a>
-
+        </div>
       </div>
     </div>
   )
