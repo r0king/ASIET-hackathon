88753d5758ed9f0da34cd7e84eb962cac0b82db0
Royal Babu <123royalbabu@gmail.com>
2022-12-31 14:51:12 +0530

    colors

--- src/componets/Menu.jsx
+++ src/componets/Menu.jsx
@@ -6,14 +6,13 @@ const Navbar24 = () => {
 
   return (
 
-      <header className="w-full bg-transparent text-gray-100 body-font mb-4 shadow-sm fixed z-50">
+      <header className={`w-full bg-transparent text-gray-100 body-font mb-4 shadow-sm fixed z-50 ${ isOpen && "bg-black/80"}`}>
 
         {/* :DESKTOP MENU */}
-        <div className="container mx-auto flex justify-end items-center py-2 px-5">
+        <div className=" mx-auto flex justify-end items-center py-2 px-5">
 
           {/* ::Burger icon standard */}
           <div className="inline-flex items-center">
-            <span className="mr-4 text-lg text-gray-500 uppercase">Menu</span>
             <Burger1 isOpen={isOpen} setIsOpen={setIsOpen} />
           </div>
 
@@ -21,11 +20,11 @@ const Navbar24 = () => {
 
         {/* :OPENED MENU */}
         {isOpen &&
-          <div className="bg-black/80 w-screen h-screen flex flex-col md:justify-center py-4 px-3  bg-opacity-95 text-base uppercase text-center font-semibold">
-            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white-500 hover:text-white hover:bg-black/30">Solutions</a>
-            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white-500 hover:text-white hover:bg-black/30">Prices</a>
-            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white-500 hover:text-white hover:bg-black/30">About</a>
-            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white-500 hover:text-white hover:bg-black/30">Contact</a>
+          <div className=" w-screen h-screen flex flex-col md:justify-center py-4 px-3  bg-opacity-95 text-base uppercase text-center font-semibold">
+            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white/40 hover:text-white ">Solutions</a>
+            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white/40 hover:text-white ">Prices</a>
+            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white/40 hover:text-white ">About</a>
+            <a href="#link" className="block px-3 py-7 rounded-md md:text-2xl text-white/40 hover:text-white ">Contact</a>
           </div>
         }
 
